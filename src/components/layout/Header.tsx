702f3d985e2fee0800c916c5d6823f4130faeaed
IL S <nicechildev@gmail.com>
2025-05-18 18:54:15 +0200

    fix: changed styles

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -6,24 +6,14 @@ export default function Header() {
       <nav className="container mx-auto flex h-14 items-center justify-between px-4 sm:px-6 lg:px-8">
         {/* Logo */}
         <Link href="/" className="text-lg font-semibold lowercase tracking-tight">
-          emploilausanne
+          emploivaud
         </Link>
 
         {/* Menu */}
         <ul className="flex items-center gap-6 text-sm font-medium">
           <li>
-            <Link href="/jobs" className="hover:opacity-90">
-              Offres
-            </Link>
-          </li>
-          <li>
-            <Link href="/a-propos" className="hover:opacity-90">
-              À&nbsp;propos
-            </Link>
-          </li>
-          <li>
-            <Link href="/contact" className="hover:opacity-90">
-              Contact
+            <Link href="/cgu" className="hover:opacity-90">
+              CGU
             </Link>
           </li>
         </ul>
